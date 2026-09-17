edc0a8f7ebcec292bf67a5e20d92f9bc2d17efdb
Colin <colin.diesh@gmail.com>
2026-09-16 21:50:21 -0400

    Sibling link

--- site/lib/site.ts
+++ site/lib/site.ts
@@ -14,5 +14,6 @@ export const SITE = {
     ['/guide/', 'Guide'],
     ['https://github.com/cmdcolin/videoskillet', 'GitHub ↗'],
     ['/privacy/', 'Privacy'],
+    ['https://cmdcolin.github.io/bender/', "videoskillet's sibling: bender ↗"],
   ],
 } as const
